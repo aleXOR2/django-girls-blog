0d42698da83e46b93f07c7615749e8fb4e976daf
Alyosha <alyosha@goodboy.com>
2018-12-11 11:29:19 +0100

    added detailed view for the post

--- blog/apps.py
+++ blog/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class BlogConfig(AppConfig):
     name = 'blog'
+    verbose_base = 'My Buggy Blog'
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+from django.urls import resolve
 
 from .models import Post
 
@@ -11,9 +13,16 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     dates_posts_made = posts.dates('created_date', 'day')
-    return render(request, 'blog/post_list.html', {'posts': posts, 'dates_posts_made': dates_posts_made})
+    return render(request, 'blog/post_list.html', {'posts': posts, 'dates_posts_made': dates_posts_made, 'app_name': request.resolver_match.app_name})
+
 
 def post_current_time(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post, 'app_name': request.resolver_match.app_name})
+
